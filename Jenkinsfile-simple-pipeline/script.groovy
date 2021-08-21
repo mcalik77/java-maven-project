7f05105847af4052088eeaa0f1a1c0d5cd70a688
mcalik77 <mcalikdevop@gmail.com>
2021-08-21 09:33:24 -0700

    added deploy option

--- Jenkinsfile-simple-pipeline/script.groovy
+++ Jenkinsfile-simple-pipeline/script.groovy
@@ -10,10 +10,16 @@ def buildImage() {
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push mcalik77/demo-app:jma-2.0'
     }
-} 
+}
 
 def deployApp() {
-    echo 'deploying the application...'
-} 
+    sshagent(['docker-server']) {
+     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+     sh "echo $PASS | docker login -u $USER --password-stdin"
+     sh 'docker pull mcalik77/demo-app:jma-2.0'
+     sh 'docker run -d -p 8081:8081 mcalik77/demo-app:jma-2.0'
+     }
+}
+}
 
 return this
