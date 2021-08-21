1aef4fdb3e354a4c28da455ac7e441d64c7715b0
mcalik77 <mcalikdevop@gmail.com>
2021-08-21 09:47:30 -0700

    fix jenkins permission

--- Jenkinsfile-simple-pipeline/script.groovy
+++ Jenkinsfile-simple-pipeline/script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t mcalik77/demo-app:jma-2.0 .'
+        sh 'docker build -t mcalik77/demo-app:jma-2.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push mcalik77/demo-app:jma-2.0'
+        sh 'docker push mcalik77/demo-app:jma-2.1'
     }
 }
 
@@ -16,8 +16,8 @@ def deployApp() {
     sshagent(['docker-server']) {
      withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
      sh "echo $PASS | docker login -u $USER --password-stdin"
-     sh 'docker pull mcalik77/demo-app:jma-2.0'
-     sh 'docker run -d -p 8081:8081 mcalik77/demo-app:jma-2.0'
+     sh 'docker pull mcalik77/demo-app:jma-2.1'
+     sh 'docker run -d -p 8081:8081 mcalik77/demo-app:jma-2.1'
      }
 }
 }
